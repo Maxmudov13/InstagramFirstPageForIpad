ecafb9362c66764b509de904872ac2389c8cb3b0
Maxmudov13 <maxmudovilyos2003@gmail.com>
2021-12-20 16:06:05 +0500

    Initial Commit

---
+++ InstagramFirstPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirstPage
+//
+//  Created by user on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
